4e0d3e5789eb60c87008fd1f70454f821cb79406
MichaelAllain <michaeldallain@gmail.com>
2018-07-03 10:59:55 -0700

    Add game info and questions
    Added structure, styling, basic JS
    for game info header and
    questions

--- public/app.js
+++ public/app.js
@@ -1,10 +1,33 @@
 'use strict';
 
+const STORE = {
+    round: 1,
+    guesses: 3,
+    totScore: 0,
+}
+
+// check and render total game score
+function updateTotScore() {
+    $('#js-tot-score').text(STORE.totScore);
+}
+
+// check and render remaining round guesses
+function updateGuesses() {
+    $('#js-guesses').text(STORE.guesses);
+}
+
+// check and render current game round
+function updateRound() {
+    $('#js-round').text(STORE.round);
+}
+
 function renderGame() {
-    console.log('rendering game');
     $('.front-header').hide();
     $('.game-header').show();
     $('.main').show();
+    updateRound();
+    updateGuesses();
+    updateTotScore();
 }
 
 function handleStartBtn() {
