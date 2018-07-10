4ad065361b59838f9e1f6c56facd9d6d6deaa829
MichaelAllain <michaeldallain@gmail.com>
2018-07-10 11:11:20 -0700

    Add next round btn and JS
    Added button to advance to
    next round and JS fxns to
    handle clicks and toggle views.
    Also added JS fxns for basic
    gameplay tasks

--- public/initialize.js
+++ public/initialize.js
@@ -2,17 +2,69 @@
 
 const STORE = {
     round: 1,
+    roundScore: 0,
     guesses: 3,
-    totScore: 0,
+    question: 'This is a placeholder question for the Survey Says game',
+    roundHistory: [
+        {
+            score:0,
+            possible:95
+        },
+        {
+            score:0,
+            possible:100
+        },
+        {
+            score:0,
+            possible:92
+        }
+    ]
 }
 
 const Model = {};
 const View = {};
 const Controller = {};
 
+Model.endRound = () => {
+    Model.storeRoundScore();
+    Model.resetGuesses();
+    Model.incRound();
+} 
+
+// decrease remaining guesses in current round
+Model.decGuesses = () => {
+    STORE.guesses -= 1;
+}
+
+// reset available guesses to 3
+Model.resetGuesses = () => {
+    STORE.guesses = 3;
+}
+
+// increment the current round
+Model.incRound = () => {
+    STORE.round += 1;
+}
+
+// store the current round score in roundHistory and zero roundScore
+Model.storeRoundScore = () => {
+    STORE.roundHistory[STORE.round - 1].score = STORE.roundScore;
+    STORE.roundScore = 0;
+}
+
+// get the total score for all combined rounds
+Model.getTotScore = () => {
+    return STORE.roundHistory.reduce((accum, elem) => (accum + elem.score), 0);
+}
+
+// get the total possible score for all combined rounds
+Model.getTotPossible = () => {
+    return STORE.roundHistory.reduce((accum, elem) => (accum + elem.possible), 0);
+}
+
 // check and render total game score
 View.updateTotScore = () => {
-    $('#tot-score').text(STORE.totScore);
+    $('#tot-score').text(Model.getTotScore());
 }
 
 // check and render remaining round guesses
@@ -25,15 +77,51 @@ View.updateRound = () => {
     $('#round').text(STORE.round);
 }
 
+View.updateQuestion = () => {
+    $('#question-text').text(STORE.question);
+}
+
+View.updateAnswers = () => {
+
+}
+
 // set up game screen by showing hiding elements and setting game variables
 View.renderGame = () => {
+    View.renderNewRound();
     $('.front-header').toggleClass('front-header--hidden');
     $('.game-header').toggleClass('game-header--hidden');
     $('.main').toggleClass('main--hidden');
+}
+
+// update DOM elements from STORE at beginning of new round
+View.renderNewRound = () => {
     View.updateRound();
     View.updateGuesses();
     View.updateTotScore();
-    Controller.handleShowMeBtn();
+    View.updateQuestion();
+    View.updateAnswers();
+}
+
+View.renderResults = () => {
+    
+} 
+
+// show/hide "Show Me..." and "Next" buttons
+View.toggleEndRound = () => {
+    $('#guess-form').toggleClass('guess-form--hidden');
+    $('#next-btn').toggleClass('btn--hidden');
+}
+
+Controller.handleNextBtn = () => {
+    $('#next-btn').click((evt) => {
+        Model.endRound();
+        if (STORE.round < 3) {
+            View.toggleEndRound();
+            View.renderNewRound();
+        } else {
+            View.renderResults();
+        }
+    });
 }
 
 Controller.handleShowMeBtn = () => {
@@ -50,7 +138,9 @@ Controller.handleStartBtn = () => {
 
 function initialize() {
     Controller.handleStartBtn();
-    // $('#js-start-btn').click() //temp
+    Controller.handleShowMeBtn();
+    Controller.handleNextBtn();
+    // $('#js-start-btn').click() //temp to skip front screen
 }
 
 $(initialize);
