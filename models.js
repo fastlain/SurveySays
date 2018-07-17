e2c033abecbf7e00ca39fce59c763e21f49ea1a3
MichaelAllain <michaeldallain@gmail.com>
2018-07-17 11:47:35 -0700

    Create question/answer model

---
+++ models.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const questAnsSchema = mongoose.Schema({
+    question: {type: String, required: true},
+    answers: [
+        {
+            display: {type: String, required: true},
+            matches: [{type: String, required: true}],
+            pts: {type: Number, required: true},
+            guessed: {type: Boolean, required: true, default: false}
+        }
+    ],
+    remainingAns: {
+        type: Number, 
+        default: function() {
+            return this.answers.length;
+        }
+    }
+});
+
+const QuestAns = mongoose.model('QuestAns', questAnsSchema, 'questans');
+
+module.exports = {QuestAns};
