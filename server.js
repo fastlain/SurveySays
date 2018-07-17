f9423ae2ac88081ac695b12810db990719ae1047
MichaelAllain <michaeldallain@gmail.com>
2018-07-17 11:50:26 -0700

    Set up Mongo(oose) and GET
    Created connection to MongoDB
    and  linked to questans model.
    Created temporary GET endpoint
    which returns number of requested
    questans documents from DB

--- server.js
+++ server.js
@@ -1,42 +1,71 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {QuestAns} = require('./models');
+
 app.use(express.static('public'));
+
+app.get('/questans/:count', (req,res) => {
+  //QuestAns.aggregate([{$sample: {size: 3}}]);
+  const count = (parseInt(req.params.count));
+  
+  QuestAns
+    .find()
+    .limit(count)
+    .then(questans => {
+      res.json(questans);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
 
 // declare `server` here, assign a value in runServer, and access it in closeServer
 let server;
 
 // Starts server and return a Promise (asynchronous for test code)
-function runServer() {
-    const port = process.env.PORT || 8080;
-    return new Promise((resolve, reject) => {
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
       server = app.listen(port, () => {
         console.log(`Your app is listening on port ${port}`);
-        resolve(server);
-      }).on('error', err => {
-        reject(err)
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
       });
-    });
-  }
+  });
+}
   
 // Closes server and return a Promise (asynchronous for test code)
-  function closeServer() {
+function closeServer() {
+  return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
       console.log('Closing server');
       server.close(err => {
         if (err) {
-          reject(err);
-          return;
+          return reject(err);
         }
         resolve();
       });
     });
-  }
+  });
+}
   
-  // call runServer if server.js is called directly (i.e. not from test code)
-  if (require.main === module) {
-    runServer().catch(err => console.error(err));
-  };
-  
-  // exports for use in test code
-  module.exports = {app, runServer, closeServer};
+// call runServer if server.js is called directly (i.e. not from test code)
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+};
+
+// exports for use in test code
+module.exports = {app, runServer, closeServer};
