3579d37ba953c1262da249b5ea808d6ebcd385e9
MichaelAllain <michaeldallain@gmail.com>
2018-07-19 16:13:54 -0700

    Add port to TEST db url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost:27017/surveysays';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                      'mongodb://localhost/test-surveysays';
+                      'mongodb://localhost:27017/test-surveysays';
 exports.PORT = process.env.PORT || 8080;
